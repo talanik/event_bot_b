b4798c9ddccf5b3f35458122faf44819981b0673
talanik <xnupam@gmail.com>
2022-03-02 14:16:50 +0500

    last

--- installer_db.py
+++ installer_db.py
@@ -6,7 +6,7 @@ from Class.db import DB
 conn = sqlite3.connect("maindb.db")
 cursor = conn.cursor()
 
-# cursor.execute("DROP TABLE orders")
+# cursor.execute("DROP TABLE users")
 cursor.execute("""CREATE TABLE if not exists localize (
                                                         alias text, 
                                                         ru text, 
@@ -54,17 +54,8 @@ cursor.execute("""CREATE TABLE if not exists agents (
                                                         fio text
                                                     )""")
 
-# cursor.execute("""UPDATE events SET sended=0 WHERE event_id=1""")
 conn.commit()
 
 
-# dd = cursor.execute("""SELECT * FROM events WHERE (event_date>1646140381 AND sended = 1 AND event_name LIKE 'Квиз-мафия%')""")
-# res = dd.fetchone()
-#
-# print(res)
-conn.close()
 
-db = DB()
-# db.insert(table='agents', columns=['agent_id','department','fio'],values=[523042204,'admin','Тимур'],closed=False)
-# db.update(table='events', sets={'event_limit': 100}, conditions={'event_id': 1}, closed=False)
-print(db.fetchall('localize', conditions={'alias':'back'}))
+conn.close()
